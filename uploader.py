59c3a5ef580c943d0ed9f4d16ed5202ca256b423
Mesut Tasci <slackermesut@gmail.com>
2017-05-05 10:32:27 +0300

    Retry upload if an error occurred

--- uploader.py
+++ uploader.py
@@ -1,6 +1,7 @@
 import os
 import math
 import sys
+import time
 
 import requests
 
@@ -16,21 +17,34 @@ def upload_file(file_path):
         chunk_count = math.ceil(file_size / config.MAX_UPLOAD_BYTE_LENGHT)
         print("Total chunk count:", chunk_count)
 
-        for i in range(chunk_count):
+        retry_timeout = 1
+        sent_chunk_count = 0
+        while True:
             end = min(file_size, start + config.MAX_UPLOAD_BYTE_LENGHT)
             headers['Range'] = "bytes={}-{}/{}".format(start, end, file_size)
-            # print(headers)
 
             file.seek(start)
             data = file.read(end)
+            start = end
 
             upload_endpoint = os.path.join(config.API_URL, 'content', 'upload')
 
-            response = requests.post(upload_endpoint, headers=headers, data=data)
-            if response.ok:
-                print('{}. chunk sent to server'.format(i + 1))
+            try:
+                response = requests.post(upload_endpoint, headers=headers, data=data)
+                if response.ok:
+                    print('{}. chunk sent to server'.format(sent_chunk_count + 1))
+                    sent_chunk_count += 1
+            except requests.exceptions.RequestException:
+                print('Error while sending chunk to server. Retrying in {} seconds'.format(retry_timeout))
+                time.sleep(retry_timeout)
 
-            start = end
+                # Sleep for max 10 seconds
+                if retry_timeout < 10:
+                    retry_timeout += 1
+                continue
+
+            if sent_chunk_count >= chunk_count:
+                break
 
 
 if __name__ == '__main__':
